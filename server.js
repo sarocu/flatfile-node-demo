2073ca5fb95f6ed8ec5b3565071ffe9016d8bdeb
sam <sam@sarocu.com>
2022-11-08 09:54:17 -0700

    chore: add partial rejection

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const jwt = require('jsonwebtoken');
 const app = express();
 
-const EMBED_ID = 'YOUR_EMBED_ID'
-const PRIVATE_KEY = 'YOUR_PRIVATE_KEY'
+const EMBED_ID = '82989a11-32d0-42e2-960d-d363e80ce944'
+const PRIVATE_KEY = 'd4JcuXbVdJlQtZv4ghDTwD0hPKHUK9iBSzMnWKCmXopYUh9TsZEGZdyodVaD6oX'
 
 app.set('view engine', 'html');
 app.set('views', __dirname);
@@ -11,11 +11,11 @@ app.engine('html', require('ejs').renderFile)
 
 app.get('/', async (_req, res) => {
   const currentUser = {
-    email: 'your.user@example.com'
+    email: 'sam+111@example.com'
   }
 
   return res.render('index.html', {
-    token: await jwt.sign({
+    token: jwt.sign({
       embed: EMBED_ID,
       sub: currentUser.email
     }, PRIVATE_KEY)
